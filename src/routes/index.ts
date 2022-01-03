36aa1383a43d0a6a1303d0ad01688bad490db3af
Vitor Lazzaretti <vitorlazzaretti@gmail.com>
2022-01-02 22:47:49 -0300

    Final Project 1.0

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,6 +9,6 @@ router.get('/dogs', PageController.dogs);
 router.get('/cats', PageController.cats);
 router.get('/fish', PageController.fish);
 
-router.get('/', SearchController.search);
+router.get('/search', SearchController.search);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(mainRoutes);
 
 server.use((req, res) => {
-    res.status(404).send('PAGE NOT FOUND');
+    res.status(404).render('pages/404');
 });
 
 server.listen(process.env.PORT);
